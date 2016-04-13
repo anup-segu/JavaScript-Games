2afb028e79764346aa69a7d950cb16f825b18484
anup-segu <anup.segu@gmail.com>
2016-04-13 10:23:47 -0700

    Add Tic Tac Toe Board View

--- ttt.js-gh-pages/skeleton/js/main.js
+++ ttt.js-gh-pages/skeleton/js/main.js
@@ -3,6 +3,8 @@ var Game = require('../../ttt-core-solution/game');
 
 $(function () {
   // Your code here
+  var game = new Game();
+  var $el = $("figure");
+  var view = new View(game, $el);
+  view.setupBoard();
 });
-
-
--- ttt.js-gh-pages/skeleton/js/ttt-view.js
+++ ttt.js-gh-pages/skeleton/js/ttt-view.js
@@ -1,4 +1,6 @@
 var View = function (game, $el) {
+  this.$el = $el;
+  this.game = game;
 };
 
 View.prototype.bindEvents = function () {
@@ -8,6 +10,16 @@ View.prototype.makeMove = function ($square) {
 };
 
 View.prototype.setupBoard = function () {
+  var ul = document.createElement("ul");
+
+  this.$el.append(ul);
+  var $ul = $(ul);
+
+  for (var i = 0; i < 9; i++) {
+    var li = document.createElement("li");
+    $ul.append(li);
+  }
+
 };
 
 module.exports = View;
